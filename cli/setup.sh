ca3dad806e064e796e14badd17e57e1d30f67c92
Manoj Bableshwar <mabables@microsoft.com>
2022-05-19 23:53:29 -0700

    testing assets in registry

--- cli/setup.sh
+++ cli/setup.sh
@@ -8,7 +8,8 @@ then
     az extension add -n ml -y
 # else install private CLI
 else
-    az extension add --source $CLI --yes
+#    az extension add --source $CLI --yes
+  az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/62707480/ml-0.0.62707480-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/62707480 --yes
 fi
 
 echo "CLI version:"
