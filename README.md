fadb86646f04ef0d2c1c87b0c6b72c2c4cd173b4
Manoj Bableshwar <mabables@microsoft.com>
2022-05-19 20:25:57 -0700

    initial workflow yaml and setup

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # AzureMLtest
+
+# AzureMLOpsDemo
+
+Install gh cmd line installed and log in.
+
+Login with: `az login` 
+
+Set your Azure Subscription: `az account set -s <sub_id>`
+
+Set $GROUP $WORKSPACE $LOCATION
+
+```
+export GROUP=<resource_group>
+
+export WORKSPACE=<workspace_name>
+
+export LOCATION=<workspace_location>
+
+```
+
+Create CLI auth secret. (Needs gh cmd line installed and logged in)
+```
+SUBSCRIPTION=$(az account show --query id -o tsv)
+
+SECRET_NAME="AZ_CREDS"
+
+az ad sp create-for-rbac --name $GROUP --role owner --scopes /subscriptions/$SUBSCRIPTION/resourceGroups/$GROUP --sdk-auth | gh secret set $SECRET_NAME
+
+```
+
---
+++ cli/setup.sh
@@ -0,0 +1,44 @@
+az extension remove -n azure-cli-ml
+az extension remove -n ml
+
+
+# if CLI env var not set, then install default CLI
+if [[ -z "$CLI" ]]
+then
+    az extension add -n ml -y
+# else install private CLI
+else
+    az extension add --source $CLI --yes
+fi
+
+# enable PRP features 
+export AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED=true 
+
+echo "CLI version:"
+az version
+
+if [[ -z "$GROUP" ]]
+then
+    echo "GROUP env var not set"
+    exit 1
+fi
+
+if [[ -z "$WORKSPACE" ]]
+then
+    echo "WORKSPACE env var not set"
+    exit 1
+fi
+
+if [[ -z "$LOCATION" ]]
+then
+    echo "LOCATION env var not set"
+    exit 1
+fi
+
+# Use defaults if not passed by workflow inputs
+
+echo "Setting defaults: group=$GROUP workspace=$WORKSPACE location=$LOCATION"
+
+az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
+
+
