60892e3836c8314b46b5fdb645922cc3c81d5917
Manoj Bableshwar <mabables@microsoft.com>
2022-05-19 23:02:13 -0700

    test component

---
+++ cli/create-job.sh
@@ -0,0 +1,59 @@
+job=$1
+set_var=$2
+
+if [[ -z "$set_var" ]]
+then
+  echo "az ml job create --file $job --query name -o tsv"
+  export run_id=$(az ml job create --file $job --query name -o tsv)
+else
+  set_var$(echo $set_var | sed 's/=/ /g')
+  echo "az ml job create --file $job --set $set_var --query name -o tsv"
+  export run_id=$(az ml job create --file $job --set $set_var --query name -o tsv)
+fi
+
+#export run_uri=$(az ml job show --name $run_id --query services.Studio.endpoint)
+export run_uri="https://ml.azure.com/runs/$run_id?flight=ModelRegisterV2,ModelRegisterExistingEnvironment,dpv2data"
+
+if [[ -z "$run_id" ]]
+then
+    echo "Job creation failed"
+    exit 3
+fi
+
+az ml job show --name $run_id
+
+status=$(az ml job show --name $run_id --query status -o tsv)
+
+if [[ -z "$status" ]]
+then
+    echo "Status query failed"
+    exit 4
+fi
+
+job_uri=$(az ml job show --name $run_id --query services.Studio.endpoint)
+
+echo $job_uri
+
+running=("Queued" "NotStarted" "Starting" "Preparing" "Running" "Finalizing")
+while [[ ${running[*]} =~ $status ]]
+do
+    echo $job_uri
+    sleep 8 
+    status=$(az ml job show --name $run_id --query status -o tsv)
+    echo $status
+done
+
+if [[ $status == "Completed" ]]
+then
+    echo "Job completed"
+    echo "::set-output name=RUNID::$run_id"
+    echo "::set-output name=RUNURI::https://ml.azure.com/runs/$run_id?flight=ModelRegisterV2,ModelRegisterExistingEnvironment,dpv2data"
+    exit 0
+elif [[ $status == "Failed" ]]
+then
+    echo "Job failed"
+    exit 1
+else
+    echo "Job not completed or failed. Status is $status"
+    exit 2
+fi   
--- cli/test-component.sh
+++ cli/test-component.sh
@@ -2,21 +2,32 @@
 
 pyml=$1
 
+
+if [[ -z "$pyml" ]]
+then
+    echo "pipeline file name missing"
+    exit 1
+fi
+
+dir=$(dirname $pyml)
+
 for job in $( yq eval '.jobs | keys' $pyml | awk  '{print $2}' )
 do
   echo "job name: $job"
   c_file=$( yq eval ".jobs.$job.component" $pyml | sed 's/file://' )
+  c_file="$dir\/$c_file"
   echo "component file: $c_file"
   c_name=$(yq eval '.name' $c_file)
   c_version=$(date +"%s")
-  echo "az ml component create --file $c_file --version $c_versio"
+  echo "az ml component create --file $c_file --version $c_version"
   az ml component create --file $c_file --version $c_version || {
       echo "Component create failed for c_file=$c_file"
       exit 1
   }
   echo "az ml component show --name $c_name --version $c_version"
   az ml component show --name $c_name --version $c_version
-  
-  
+  set_var="$set_var=jobs.$job.component=azureml:$c_name:$c_version"
 done
+
+bash ./create-job.sh $pyml $set_var
   
