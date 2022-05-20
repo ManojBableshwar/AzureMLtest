f138d3f29185e40f831d38bf6734c4b9e0eed26f
Manoj Bableshwar <mabables@microsoft.com>
2022-05-20 14:12:44 -0700

    create builtin samples

--- cli/list-component-builtin.sh
+++ cli/list-component-builtin.sh
@@ -1,8 +1,6 @@
 mode=$1
-
 unixepoch=$(date +"%s")
 version=$(( $unixepoch + $RANDOM ))
-
 if [[ $mode == "registry" ]]
 then
   if [[ -z "$REGISTRY" ]]
@@ -34,25 +32,14 @@ az ml environment show --name publicimageexample --version $version $reg_var  ||
     exit 1
 }
 
-c_file="../config/component.yml"
-c_version=$version
-
 while read line
 do
   c_name=$(echo $line | awk -F, '{print $1}')
-  c_display_name=$(echo $line | awk -F, '{print $2}')
-  echo "az ml component create --file $c_file --name $c_name --version $c_version --set display_name=\"$c_display_name\" environment=azureml://registries/$REGISTRY/environments/publicimageexample/labels/latest $reg_var "
-  start_time=`date +%s`
-  az ml component create --file $c_file --name $c_name --version $c_version --set display_name="$c_display_name" environment=azureml://registries/$REGISTRY/environments/publicimageexample/labels/latest $reg_var  || {
-      echo "Component create failed for c_name=$c_name"
-      exit 1
-  }
-  end_time=`date +%s`
-  timetaken=$(expr $end_time - $start_time)
-  echo "Time taken: $timetaken seconds"
-  echo "az ml component show --name $c_name --version $c_version $reg_var"
-  az ml component show --name $c_name --version $c_version $reg_var || {
-      echo "Component show failed for --name $c_name --version $c_version"
-      exit 1
-  }
+  matrix="$matrix,\"$c_name\""
 done < ../config/component.csv
+
+matrix=$(echo $matrix | sed 's/,//')
+
+echo "matrix=$matrix"
+
+echo "::set-output name=sample_matrix::[$matrix]"
---
+++ cli/populate-assets-parallel.sh
@@ -0,0 +1,37 @@
+c_name=$1
+mode=$2
+
+unixepoch=$(date +"%s")
+version=$(( $unixepoch + $RANDOM ))
+
+if [[ $mode == "registry" ]]
+then
+  if [[ -z "$REGISTRY" ]]
+  then 
+    echo "mode=$mode but REGISTRY is not set"
+    exit 1
+  else
+    echo "Using REGISTRY=$REGISTRY to create assets..."
+    reg_var="--registry-name $REGISTRY"
+  fi
+else
+  echo "Using WORKSPACE=$WORKSPACE to create assets..."
+fi
+
+c_version=$version
+
+c_display_name=$(cat ../config/component.csv | grep "$c_name" | awk -F, '{print $2}')
+echo "az ml component create --file $c_file --name $c_name --version $c_version --set display_name=\"$c_display_name\" environment=azureml://registries/$REGISTRY/environments/publicimageexample/labels/latest $reg_var "
+start_time=`date +%s`
+az ml component create --file $c_file --name $c_name --version $c_version --set display_name="$c_display_name" environment=azureml://registries/$REGISTRY/environments/publicimageexample/labels/latest $reg_var  || {
+    echo "Component create failed for c_name=$c_name"
+    exit 1
+}
+end_time=`date +%s`
+timetaken=$(expr $end_time - $start_time)
+echo "Time taken: $timetaken seconds"
+echo "az ml component show --name $c_name --version $c_version $reg_var"
+az ml component show --name $c_name --version $c_version $reg_var || {
+  echo "Component show failed for --name $c_name --version $c_version"
+  exit 1
+}
